fb0e0f9a5b56983ab1febf9d976cc9004d46828e
smitprajapati407 <prajapatismit407@gmail.com>
2025-12-30 16:22:54 +0530

    Add home route for root URL

--- first/first/urls.py
+++ first/first/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from  hospital import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('hospital.urls'))
+    path('',include('hospital.urls')),
+     path('', views.home, name='home'),
 ]
